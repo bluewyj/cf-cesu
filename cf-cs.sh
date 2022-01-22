4dda8d10b9fdf0703a5973813ec7dc715c9c2cf6
bluewyj <59365597+bluewyj@users.noreply.github.com>
2022-01-22 23:20:29 +0800

    Update cf-cs.sh

--- cf-cs.sh
+++ cf-cs.sh
@@ -1,6 +1,4 @@
-cd /root/CloudflareST
-
 chmod +x CloudflareST
 
 # 运行 CloudflareST 测速（脚本示例中的 CloudflareST 位于 /root/CloudflareST 目录下）
-./CloudflareST -tp 443 -tll 90 -t 20 -sl 5
+./CloudflareST -tp 443 -tll 90 -sl 5
